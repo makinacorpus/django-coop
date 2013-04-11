3efc222ca7f73eb6b0d1b0c19fb4c8cccd88e9b8
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-04-11 09:30:31 +0200

    Add exchange plugin

---
+++ ionyweb_plugins/plugin_coop_exchange/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext as _
+
+PLUGIN_INFO = {
+    'NAME': _(u"Coop exchange"),
+    'CATEGORY': 'other',
+    'VERSION': "1.0",
+    'SHORT_DESCRIPTION': _(u"Display the lastest exchanges from django-coop exchange"),
+    'DESCRIPTION': _(u"Display the lastest exchanges from django-coop exchange. This plugin requires the App coop_exchanges to be installed."),
+}
