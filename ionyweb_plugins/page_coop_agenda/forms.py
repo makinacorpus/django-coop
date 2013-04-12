10b710147caa8e97b63994ac42ea7a1e533fe8ec
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-04-12 16:58:31 +0200

    Agenda search form

--- ionyweb_plugins/page_coop_agenda/forms.py
+++ ionyweb_plugins/page_coop_agenda/forms.py
@@ -3,8 +3,18 @@
 import floppyforms as forms
 from ionyweb.forms import ModuloModelForm
 from .models import PageApp_CoopAgenda
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 class PageApp_CoopAgendaForm(ModuloModelForm):
 
+    location = forms.CharField(required=False, label=_('Location'))
+    location_buffer = forms.IntegerField(required=False, label=_('Location buffer'))
+    free_search = forms.CharField(required=False, label=_('Free search'))
+    activity = forms.CharField(required=False, label=_('Activity'))
+    organization = forms.CharField(required=False, label=_('Organization'))
+    start_date = forms.DateField(required=False, label=_('Start date'))
+    end_date = forms.DateField(required=False, label=_('End date'))
+    type = forms.CharField(required=False, label=_('Type'))
+    
     class Meta:
         model = PageApp_CoopAgenda
