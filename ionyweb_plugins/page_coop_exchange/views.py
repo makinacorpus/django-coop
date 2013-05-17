89e91597a854af06a2382553fdf2c56103a64a15
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-05-17 16:01:04 +0200

    Autocomplete for location

--- ionyweb_plugins/page_coop_exchange/views.py
+++ ionyweb_plugins/page_coop_exchange/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext
 from ionyweb.website.rendering.utils import render_view
 
 from coop_local.models import Exchange
+from coop_local.models import Location
 
 from django.conf import settings
 
@@ -19,6 +20,7 @@ from django.db.models import Q
 from django.contrib.gis import geos
 from coop_local.models import Location
 from math import pi
+from django.utils.simplejson import dumps
 
 
 from coop.exchange.admin import ExchangeForm
@@ -27,56 +29,8 @@ MEDIAS = (
     CSSMedia('page_coop_exchange.css'),
 )
 
-def index_view(request, page_app):
-    
+def index_view(request, page_app):    
     rdict = filter_data(request, page_app)
-    
-    #base_url = u'%s' % (page_app.get_absolute_url())
-
-    #exchanges = Exchange.objects.all()
-
-    #if request.method == 'POST': # If the form has been submitted        
-        #form = PageApp_CoopExchangeForm(request.POST)
-        #if form.is_valid():
-            #if form.cleaned_data['free_search']:
-                #exchanges = exchanges.filter(Q(title__contains=form.cleaned_data['free_search']) | Q(description__contains=form.cleaned_data['free_search']))
-            
-            #if form.cleaned_data['type_exchange']:
-                #exchanges = exchanges.filter(Q(eway__in=form.cleaned_data['type_exchange']))
-
-            #if form.cleaned_data['type']:
-                #exchanges = exchanges.filter(Q(etype__in=form.cleaned_data['type']))
-
-            #if form.cleaned_data['activity']:
-                #exchanges = exchanges.filter(Q(activity=form.cleaned_data['activity']))
-            
-            #if form.cleaned_data['thematic']:
-                #exchanges = exchanges.filter(Q(transverse_themes=form.cleaned_data['thematic']))
-            
-            #if form.cleaned_data['location']:
-                #coords = form.cleaned_data['location'].split(",")
-                #center = geos.Point(float(coords[0]), float(coords[1]))
-                #radius = form.cleaned_data['location_buffer']
-                #distance_degrees = (360 * radius) / (pi * 6378)
-                #zone = center.buffer(distance_degrees)
-                
-                 ## Get the possible location in the buffer...
-                #possible_locations = Location.objects.filter(point__intersects=zone)
-                ## ...and filter organization according to these locations
-                #exchanges = exchanges.filter(Q(location__in=possible_locations))
-            
-            ##TODO : mode
-            
-            ##TODO : skills
-            
-    #else:
-        #form = PageApp_CoopExchangeForm({'location_buffer': '10'}) # An empty form
-        #more_criteria = False
-    
-    #center_map = settings.COOP_MAP_DEFAULT_CENTER
-    
-    #rdict = {'exchanges': exchanges, 'base_url': base_url, 'form': form, 'center': center_map, 'more_criteria': more_criteria}
-    
     return render_view('page_coop_exchange/index.html',
                        rdict,
                        MEDIAS,
@@ -116,8 +70,10 @@ def filter_data(request, page_app):
                 exchanges = exchanges.filter(Q(transverse_themes=form.cleaned_data['thematic']))
             
             if form.cleaned_data['location']:
-                coords = form.cleaned_data['location'].split(",")
-                center = geos.Point(float(coords[0]), float(coords[1]))
+                label = form.cleaned_data['location']
+                location = get_object_or_404(Location, label=label)
+                
+                center = geos.Point(float(location.point.x), float(location.point.y))
                 radius = form.cleaned_data['location_buffer']
                 distance_degrees = (360 * radius) / (pi * 6378)
                 zone = center.buffer(distance_degrees)
@@ -137,7 +93,10 @@ def filter_data(request, page_app):
     
     center_map = settings.COOP_MAP_DEFAULT_CENTER
     
-    rdict = {'exchanges': exchanges, 'base_url': base_url, 'form': form, 'center': center_map, 'more_criteria': more_criteria}
+    # Get available locations for autocomplete
+    available_locations = dumps([location.label for location in Location.objects.all()])
+    
+    rdict = {'exchanges': exchanges, 'base_url': base_url, 'form': form, 'center': center_map, 'more_criteria': more_criteria, 'available_locations': available_locations}
     
     return rdict
                        
