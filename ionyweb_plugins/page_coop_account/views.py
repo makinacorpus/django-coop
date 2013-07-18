a72ee5473ba467ce47637af60687be3f4d2507e7
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-07-18 14:43:58 +0200

    My account list objets

--- ionyweb_plugins/page_coop_account/views.py
+++ ionyweb_plugins/page_coop_account/views.py
@@ -3,7 +3,7 @@
 from django.template import RequestContext
 from ionyweb.website.rendering.utils import render_view
 
-from coop_local.models import Organization, Event, EventCategory, Calendar, Occurrence
+from coop_local.models import Organization, Event, EventCategory, Calendar, Occurrence, Exchange
 from django.conf import settings
 
 from django.shortcuts import get_object_or_404
@@ -22,7 +22,7 @@ from coop_local.models import Location
 from django.contrib.auth import authenticate, login, logout
 
 from coop.org.models import get_rights as get_rights_org
-
+from ionyweb_plugins.page_coop_blog.models import CoopEntry
 
 MEDIAS = (
     CSSMedia('page_coop_account.css'),
@@ -37,41 +37,74 @@ def index_view(request, page_app):
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                login(request, user)
-                # Redirect to a success page.
-            #else:
-                # Return a 'disabled account' error message
-    #else:
-        # Return an 'invalid login' error message.
-        
+                login(request, user)        
     
     tab_org = []
+    tab_projects = []
+    tab_exchanges = []
+    tab_events = []
+    tab_entries = []
+
     if request.user.is_authenticated():
         render_page = 'page_coop_account/index.html'
-        
-        
-        # TODO: gestion infos personnelles
         
         # My organizations
         organizations = Organization.objects.filter(is_project=False).order_by('title')
         for o in organizations:
-            print o.pk
-            can_edit, can_add = get_rights_org(request, o.pk)
+            if request.user.is_superuser:
+                can_edit = True
+                can_add = True
+            else:
+                can_edit, can_add = get_rights_org(request, o.pk)
             if can_edit:
                 tab_org.append(o)
+
+        # My projects
+        projects = Organization.objects.filter(is_project=True).order_by('title')
+        for o in projects:
+            if request.user.is_superuser:
+                can_edit = True
+                can_add = True
+            else:
+                can_edit, can_add = get_rights_org(request, o.pk)
+            if can_edit:
+                tab_projects.append(o)
+
+        # My news
+        if request.user.is_superuser:
+            entries = CoopEntry.objects.all().order_by('title')
+        else:
+            entries = CoopEntry.objects.filter(author=request.user).order_by('title')
+        for e in entries:
+            tab_entries.append(e)
         
+        # My exchanges
+        exchanges = Exchange.objects.all().order_by('title')
+        for e in exchanges:
+            if request.user.is_superuser:
+                can_edit = True
+                can_add = True
+            else:
+                can_edit, can_add = get_rights_org(request, o.pk)
+            if can_edit:
+                tab_exchanges.append(e)
         
-        # TODO: gestion mes annonces / déposer une annonce / supprimer / valider..
-        
-        
-        # TODO: gestion mes évènements / déposer ...
         # My events
-        
+        occs = Occurrence.objects.all().order_by('start_time')
+        for o in occs:
+            if request.user.is_superuser:
+                can_edit = True
+                can_add = True
+            else:
+                can_edit, can_add = get_rights_org(request, o.event.pk)
+            if can_edit:
+                tab_events.append(o)
+
     else:
         render_page = 'page_coop_account/login.html'
     
         
-    rdict = {'object': page_app, 'base_url': base_url, 'org': tab_org}
+    rdict = {'object': page_app, 'base_url': base_url, 'org': tab_org, 'projects': tab_projects, 'exchanges': tab_exchanges, 'occs': tab_events, 'entries': tab_entries}
     
     return render_view(render_page,
                        rdict,
