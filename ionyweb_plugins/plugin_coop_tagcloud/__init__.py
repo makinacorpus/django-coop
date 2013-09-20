75dd9da0b89cc830a2beaca1b4f86a84f541f2ef
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-09-20 17:07:24 +0200

    Add plugin tag cloud

---
+++ ionyweb_plugins/plugin_coop_tagcloud/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext as _
+
+PLUGIN_INFO = {
+    'NAME': _(u"Coop Tag Cloud"),
+    'CATEGORY': 'other',
+    'VERSION': "1.0",
+    'SHORT_DESCRIPTION': _(u"Tag Cloud"),
+    'DESCRIPTION': _(u"Tag Cloud"),
+}
