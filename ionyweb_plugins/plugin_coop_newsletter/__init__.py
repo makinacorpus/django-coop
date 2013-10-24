ae8687ad2fb0b32253e69c603db016a0ac6fe263
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-10-24 17:56:24 +0200

    Useless plugin

--- ionyweb_plugins/plugin_coop_newsletter/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.utils.translation import ugettext as _
-
-PLUGIN_INFO = {
-    'NAME': _(u"Newsletter"),
-    'VERSION': "1.0",
-    'SHORT_DESCRIPTION': _(u"Newsletter form"),
-    'DESCRIPTION': _(u"Newsletter form"),
-}
--- ionyweb_plugins/plugin_coop_newsletter/static/admin/js/plugin_coop_newsletter_actions.js
+++
@@ -1,7 +0,0 @@
-admin.plugin_coop_newsletter = {
-    edit_guests : function(relation_id){
-            admin.GET({
-                    url : '/wa/action/' + relation_id + '/guest_list/',
-            });
-    },
-}
