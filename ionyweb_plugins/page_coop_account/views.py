4046e36868080da0c9dc5f974bd45ec3d302de41
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-11-15 12:17:07 +0100

    User preferences write nb of days for notifications

--- ionyweb_plugins/page_coop_account/views.py
+++ ionyweb_plugins/page_coop_account/views.py
@@ -135,7 +135,7 @@ def index_view(request, page_app):
     blog_url = settings.COOP_BLOG_URL
     projects_url = settings.COOP_MEMBER_PROJECTS_URL
 
-    rdict = {'object': page_app, 'base_url': base_url, 'edit_url': edit_url, 'org': tab_org, 'projects': tab_projects, 'exchanges': tab_exchanges, 'occs': tab_events, 'entries': tab_entries, 'private_entries': tab_private_entries, 'logo': logo, 'media_path': settings.MEDIA_URL, 'person': person, 'items': user_pref_matches, 'exchanges_url': exchanges_url, 'organizations_url': organizations_url, 'agenda_url': agenda_url, 'blog_url': blog_url, 'projects_url': projects_url}
+    rdict = {'object': page_app, 'base_url': base_url, 'edit_url': edit_url, 'org': tab_org, 'projects': tab_projects, 'exchanges': tab_exchanges, 'occs': tab_events, 'entries': tab_entries, 'private_entries': tab_private_entries, 'logo': logo, 'media_path': settings.MEDIA_URL, 'person': person, 'items': user_pref_matches, 'exchanges_url': exchanges_url, 'organizations_url': organizations_url, 'agenda_url': agenda_url, 'blog_url': blog_url, 'projects_url': projects_url, 'delta_days': settings.NOTIFICATION_MY_ACCOUNT_DELTA}
     
     return render_view(render_page,
                        rdict,
