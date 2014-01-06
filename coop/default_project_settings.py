fa1f4cfbee75c7bb5fde95bd752f73bb311798f2
Gael Utard <gael.utard@makina-corpus.com>
2014-01-06 10:46:16 +0100

    Bug #1212: resizable tinyMCE editor in back office

--- coop/default_project_settings.py
+++ coop/default_project_settings.py
@@ -259,26 +259,6 @@ TAGGER_FKEY_NAME = 'coop_local.Tag'
 THUMBNAIL_FORMAT = 'PNG'
 ADMIN_THUMBS_SIZE = '60x60'
 
-# From coop
-#TINYMCE_DEFAULT_CONFIG = {
-    #'theme': "advanced",
-    #'relative_urls': False,
-    #'width': '617px', 'height': '220px',
-    #'theme_advanced_toolbar_location': 'top',
-    #'theme_advanced_buttons1': 'bold,italic,|,justifyleft,justifycenter,justifyright,|,bullist,numlist,|,link,unlink,|,code',
-    #'theme_advanced_buttons2': '', 'theme_advanced_buttons3': ''
-    #}
-
-# From ionyweb
-TINYMCE_DEFAULT_CONFIG = {
-    'plugins': "table,spellchecker,filemanager,paste,searchreplace,inlinepopups",
-    'theme': "advanced",
-    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontsizeselect,|,forecolor,backcolor,|,bullist,numlist,|,outdent,indent,|,sub,sup,|,charmap,emotions,separator,forecolor,backcolor",
-    'theme_advanced_buttons2' : "pastetext,pasteword,selectall,|,undo,redo,|,link,unlink,anchor,image,filemanager,code,|,tablecontrols,|,fullscreen",
-    'theme_advanced_buttons3' : "",
-    'relative_urls': False
-}
-
 
 FORMS_BUILDER_USE_SITES = False
 
@@ -437,7 +417,11 @@ TINYMCE_DEFAULT_CONFIG = {
     'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontsizeselect,|,forecolor,backcolor,|,bullist,numlist,|,outdent,indent,|,sub,sup,|,charmap,emotions,separator,forecolor,backcolor",
     'theme_advanced_buttons2' : "pastetext,pasteword,selectall,|,undo,redo,|,link,unlink,anchor,image,filemanager,code,|,tablecontrols,|,fullscreen",
     'theme_advanced_buttons3' : "",
-    'relative_urls': False
+    'relative_urls': False,
+    'theme_advanced_resizing': True,
+    'theme_advanced_statusbar_location': 'bottom',
+    'theme_advanced_resize_horizontal': False,
+    'theme_advanced_path': False,
 }
 
 
